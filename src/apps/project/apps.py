d1203cab8f04099a61e92cc46f4ba37b8847456c
hamedkhodami <hamedkhodami977@gmail.com>
2025-02-15 22:45:52 +0330

    update1: appname(project)

--- src/apps/project/apps.py
+++ src/apps/project/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class ProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.project'
+    verbose_name = _('Project')
